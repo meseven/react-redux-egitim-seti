d050766ccb0357d66a2a47478ccc6521c52b25e2
Mehmet Seven <mehmetseven0@gmail.com>
2018-04-24 15:40:07 +0300

    es6 modul sistemi

---
+++ es6-modul-sistemi/index.js
@@ -0,0 +1,14 @@
+import kareAl, { topla as add, cikar as c } from './myModule';
+
+import * as myModule from './myModule';
+
+//console.log(topla(1, 2));
+//console.log(cikar(5, 2));
+
+//console.log(kareAl(5));
+
+//console.log(add(1, 2));
+//console.log(c(5, 2));
+
+console.log(myModule.default(4));
+//console.log(myModule.cikar(4, 2));
---
+++ es6-modul-sistemi/myModule.js
@@ -0,0 +1,7 @@
+export const topla = (a, b) => a + b;
+
+export const cikar = (a, b) => a - b;
+
+const kareAl = a =>  a * a;
+
+export default kareAl;
