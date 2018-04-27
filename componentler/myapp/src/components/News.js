4299d6d90b5d75fb20d237e14daa64aab9ff8c06
Mehmet Seven <mehmetseven0@gmail.com>
2018-04-27 21:04:56 +0300

    prop types

--- componentler/myapp/src/components/News.js
+++ componentler/myapp/src/components/News.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import NewsItem from './NewsItem';
 
 class News extends React.Component{
+	static propTypes = {
+		news: PropTypes.array.isRequired,
+	};
+
 	render(){
+		const elements = this.props.news.map(news =>
+			<NewsItem
+				key={news.id}
+				title={news.title}
+				description={news.description} />
+		);
+
 		return(
 			<div>
-				{
-					this.props.news.map(news =>
-						<NewsItem
-							title={news.title}
-							description={news.description} />
-					)
-				}
+				{ this.props.name }
+				{ elements }
 			</div>
 		)
 	}
