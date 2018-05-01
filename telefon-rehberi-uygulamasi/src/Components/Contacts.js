8505aec8752ad29f152082366132a448c5648631
Mehmet Seven <mehmetseven0@gmail.com>
2018-05-01 17:59:34 +0300

    contacts state

--- telefon-rehberi-uygulamasi/src/Components/Contacts.js
+++ telefon-rehberi-uygulamasi/src/Components/Contacts.js
@@ -3,10 +3,20 @@ import List from './List';
 import Form from './Form';
 
 class Contacts extends Component {
+	state = {
+		contacts: [{
+			name: 'Mehmet',
+			phone: '123131231'
+		},{
+			name: 'Mesut',
+			phone: '9384590834905'
+		}]
+	};
+
 	render() {
 		return (
 			<div>
-				<List/>
+				<List contacts={this.state.contacts}/>
 				<Form/>
 			</div>
 		);
--- telefon-rehberi-uygulamasi/src/Components/List.js
+++ telefon-rehberi-uygulamasi/src/Components/List.js
@@ -1,27 +1,26 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './List.css';
 
 class List extends Component {
+	static propTypes = {
+		contacts: PropTypes.array.isRequired,
+	};
+
 	render() {
 		return (
 			<div className={"listArea"}>
 				<input name={"filter"} id={"filter"} placeholder={"Filter by name or phone"}/>
 				<ul className={"list"}>
-					<li>
-						<span className={"name"}>Mehmet Seven</span>
-						<span className={"phone"}>1111111111</span>
-						<span className={"clearfix"}></span>
-					</li>
-					<li>
-						<span className={"name"}>Ercan Yıldırım</span>
-						<span className={"phone"}>33333</span>
-						<span className={"clearfix"}></span>
-					</li>
-					<li>
-						<span className={"name"}>Tayfun Erbilen</span>
-						<span className={"phone"}>22222222</span>
-						<span className={"clearfix"}></span>
-					</li>
+					{
+						this.props.contacts.map(contact =>
+							<li key={contact.phone}>
+								<span className={"name"}>{contact.name}</span>
+								<span className={"phone"}>{contact.phone}</span>
+								<span className={"clearfix"}></span>
+							</li>
+						)
+					}
 				</ul>
 			</div>
 		);
