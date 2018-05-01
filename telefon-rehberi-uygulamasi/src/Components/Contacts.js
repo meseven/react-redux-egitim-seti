ff3a7a1bf6cfd468c5ee325ba27b1e13a9fc05fc
Mehmet Seven <mehmetseven0@gmail.com>
2018-05-01 17:20:33 +0300

    stateless components list,contacts

--- telefon-rehberi-uygulamasi/src/Components/Contacts.js
+++ telefon-rehberi-uygulamasi/src/Components/Contacts.js
@@ -1,18 +1,12 @@
 import React, {Component} from 'react';
 import List from './List';
 
-class Contacts extends Component {
-	state = {
-		
-	};
-
-	render() {
-		return (
-			<div>
-				<List/>
-			</div>
-		);
-	}
-}
+const Contacts = () => {
+	return (
+		<div>
+			<List/>
+		</div>
+	);
+};
 
 export default Contacts;
