19d04a1c814681fd845d81a641d805813601b212
Mehmet Seven <mehmetseven0@gmail.com>
2018-05-01 14:10:30 +0300

    re render

--- state/src/App.js
+++ state/src/App.js
@@ -3,16 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+	state = {
+		name: 'mehmet'
+	};
+
+	changeName (){
+		this.setState({
+			name: 'ahmet' + Math.floor(Math.random() * 3)
+		});
+		console.log(this.state);
+	}
+
+	render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+				<h1>{ this.state.name }</h1>
+        <button onClick={ this.changeName.bind(this) }>Change the name</button>
       </div>
     );
   }
