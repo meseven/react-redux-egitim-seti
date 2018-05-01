a982cf682b029ee60de393f3d4b3d4cdaf15ae79
Mehmet Seven <mehmetseven0@gmail.com>
2018-05-01 14:32:36 +0300

    counter component

--- state/src/App.js
+++ state/src/App.js
@@ -2,27 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Counter from './Counter';
+
 class App extends Component {
-	state = {
-		name: 'mehmet'
-	};
-
-	changeName (){
-		this.setState({
-			name: 'ahmet' + Math.floor(Math.random() * 3)
-		});
-		console.log(this.state);
-	}
-
 	render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-				<h1>{ this.state.name }</h1>
-        <button onClick={ this.changeName.bind(this) }>Change the name</button>
+				<Counter/>
       </div>
     );
   }
