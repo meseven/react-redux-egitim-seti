d2e9c825edb09cdda129806289e60e6cb40e95e7
Mehmet Seven <mehmetseven0@gmail.com>
2018-05-02 16:08:11 +0300

    componentWillMount()

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
+
+  state = {
+    name: 'Mehmet'
+  };
+
+	constructor(props) {
+		super();
+		console.log("constructor");
+
+		this.state = {
+		  name: 'Kenan'
+    }
+	}
+
+	componentWillMount() {
+    console.log('componentWillMount');
+	}
+
+	render() {
+	  console.log('render');
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { this.state.name }
       </div>
     );
   }
