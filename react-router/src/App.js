54641ce3b784be59dfc514c40a57593a3b4dab24
Mehmet Seven <mehmetseven0@gmail.com>
2018-05-15 16:49:55 +0300

    BrowserRouter, Route

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+			<Router>
+        <div>
+					<Route path="/" exact render={
+            () => {
+              return(<h1>Home page</h1>)
+            }
+          } />
+
+					<Route path="/contact" exact render={
+						() => {
+							return(<h1>Contact page</h1>)
+						}
+					} />
+        </div>
+      </Router>
     );
   }
 }
