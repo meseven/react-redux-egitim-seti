6e84f39c9872b480c7b60cc89d732bbfff1f52d1
Mehmet Seven <mehmetseven0@gmail.com>
2018-05-22 01:51:54 +0300

    connect

--- redux/src/App.js
+++ redux/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { connect } from 'react-redux';
+
 class App extends Component {
   render() {
     return (
@@ -18,4 +20,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
