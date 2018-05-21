7b6e942031cb9f414d4723b6c5a8e1c0d3b38b28
Mehmet Seven <mehmetseven0@gmail.com>
2018-05-22 00:03:18 +0300

    dispatch

--- redux/src/index.js
+++ redux/src/index.js
@@ -6,11 +6,25 @@ import registerServiceWorker from './registerServiceWorker';
 
 import { createStore } from 'redux';
 
-function reducer() {
+function reducer(state, action) {
+	if (action.type === 'changeTheState') {
+	  return action.payload.newState
+	}
 	return 'state';
 }
+
 const store = createStore(reducer);
 
+console.log(store.getState());
+
+const action = {
+	type: 'changeTheState',
+	payload: {
+		newState: 'my new state'
+	}
+};
+
+store.dispatch(action);
 console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
