98da7556a76e16ac7478f996b86ee685a41b2174
Mehmet Seven <mehmetseven0@gmail.com>
2018-05-23 17:08:08 +0300

    immutable vs mutable

--- advanced-redux/src/index.js
+++ advanced-redux/src/index.js
@@ -4,5 +4,50 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore } from 'redux';
+
+const initialState = {
+	count: 1,
+	users: []
+};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type){
+		case "ADD":
+			return {
+				...state,
+				count: state.count + action.payload
+			};
+		case "SUBTRACT":
+			return {
+				...state,
+				count: state.count - action.payload
+			};
+		default:
+			return state;
+	}
+};
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+	console.log('store updated!', store.getState());
+});
+
+store.dispatch({
+	type: "ADD",
+	payload: 1
+});
+
+store.dispatch({
+	type: "ADD",
+	payload: 10
+});
+
+store.dispatch({
+	type: "SUBTRACT",
+	payload: 5
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
