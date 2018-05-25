1199c0298b88aaffe8dd0793f06e8374b3262f69
Mehmet Seven <mehmetseven0@gmail.com>
2018-05-25 05:25:02 +0300

    MoviesPage redux store connection

--- react-redux-movie-app/src/components/pages/MoviesPage.js
+++ react-redux-movie-app/src/components/pages/MoviesPage.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class MoviesPage extends Component {
+	static propTypes = {
+		movies: PropTypes.array.isRequired
+	};
+
 	render() {
+		console.log(this.props);
 		return (
 			<div>
 				<h2>Movies Page</h2>
@@ -10,4 +17,10 @@ class MoviesPage extends Component {
 	}
 }
 
-export default MoviesPage;
+const mapStateToProps = ({ movies }) => {
+	return {
+		movies
+	}
+};
+
+export default connect(mapStateToProps)(MoviesPage);
