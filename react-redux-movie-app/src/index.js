9b650ef53fc6131942e5cf194200f26d2785c512
Mehmet Seven <mehmetseven0@gmail.com>
2018-05-25 04:10:59 +0300

    thunk middleware & redux dev tools

--- react-redux-movie-app/src/index.js
+++ react-redux-movie-app/src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 const store = createStore(
-	rootReducer
+	rootReducer,
+	composeWithDevTools(
+		applyMiddleware(thunk)
+	)
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
