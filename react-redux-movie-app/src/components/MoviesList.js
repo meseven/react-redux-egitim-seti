2045ba35db2cfe6d1cccc297cac066a82cdfe746
Mehmet Seven <mehmetseven0@gmail.com>
2018-05-28 02:01:00 +0300

    movies list

--- react-redux-movie-app/src/components/MoviesList.js
+++ react-redux-movie-app/src/components/MoviesList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-import { Grid } from 'semantic-ui-react'
 
 const MoviesList = ({ movies }) => {
 	const emptyMessage = (
