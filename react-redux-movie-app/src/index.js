b05f40a869dc730aa74447200743548d3e4156bd
Mehmet Seven <mehmetseven0@gmail.com>
2018-05-28 00:55:23 +0300

    redux logger installation

--- react-redux-movie-app/src/index.js
+++ react-redux-movie-app/src/index.js
@@ -6,6 +6,8 @@ import registerServiceWorker from './registerServiceWorker';
 
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 import rootReducer from './reducers/rootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
@@ -14,7 +16,7 @@ import { BrowserRouter } from 'react-router-dom';
 const store = createStore(
 	rootReducer,
 	composeWithDevTools(
-		applyMiddleware(thunk)
+		applyMiddleware(thunk, logger)
 	)
 );
 
