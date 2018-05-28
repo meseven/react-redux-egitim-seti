c9bf577b5aee3af56a2e2e47fc5e4f3168cdc8de
Mehmet Seven <mehmetseven0@gmail.com>
2018-05-28 19:11:19 +0300

    NewMovieForm component

--- react-redux-movie-app/src/components/NewMovieForm.js
+++ react-redux-movie-app/src/components/NewMovieForm.js
@@ -1,10 +1,37 @@
 import React, {Component} from 'react';
+import { Button, Form, Image } from 'semantic-ui-react'
 
 class NewMovieForm extends Component {
+	state = {
+		title: '',
+		cover: ''
+	};
+
 	render() {
 		return (
 			<div>
-				<h2>new movie form</h2>
+				<h2>New Movie</h2>
+				<Form>
+					<Form.Field>
+						<label>Title</label>
+						<input
+							id="title"
+							value={this.state.title}
+							onChange={this.handeChange}
+							placeholder='Title' />
+					</Form.Field>
+					<Form.Field>
+						<label>Cover Url</label>
+						<input
+							id="cover"
+							value={this.state.cover}
+							onChange={this.handeChange}
+							placeholder='Cover Url' />
+					</Form.Field>
+					<Image src='/assets/images/wireframe/image.png' size='small' />
+					<div className="clearfix"></div>
+					<Button type='submit'>Submit</Button>
+				</Form>
 			</div>
 		);
 	}
