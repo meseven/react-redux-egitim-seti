314982249e5f9170c578ea7955acbe7391510667
Mehmet Seven <mehmetseven0@gmail.com>
2018-06-04 18:04:51 +0300

    nextjs built-in css support

--- nextjs-ssr/components/UserList.js
+++ nextjs-ssr/components/UserList.js
@@ -3,12 +3,19 @@ import Link from 'next/link';
 const UserList = () => (
 	<div>
 		<Link href="/user?name=mehmet" as="mehmet">
-			<a>Mehmet</a>
+			<a className="username">Mehmet</a>
 		</Link>
 		<br/>
 		<Link href="/user?name=ahmet" as="ahmet">
-			<a>Ahmet</a>
+			<a className="username">Ahmet</a>
 		</Link>
+
+		<style jsx>{`
+				.username{
+					background: black;
+					color: white
+				}
+		`}</style>
 	</div>
 );
 
