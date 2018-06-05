631be1bd9363f0841413b730439891ffb5f9673b
Mehmet Seven <mehmetseven0@gmail.com>
2018-06-05 12:14:50 +0300

    onMouseEnter prefetch nextjs

--- nextjs-ssr/components/UserList.js
+++ nextjs-ssr/components/UserList.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Router from 'next/router';
 
 const UserList = props => (
 	<div>
@@ -7,7 +8,9 @@ const UserList = props => (
 			props.users.map(user => (
 				<div>
 					<Link href={`/user?name=${user.username}`} as={`/@/${user.username}`}>
-						<a className="username">{ user.name }</a>
+						<a
+							onMouseEnter={() => { Router.prefetch(`/user?name=${user.username}`); console.log('prefetching..')}}
+							className="username">{ user.name }</a>
 					</Link>
 				</div>
 			))
