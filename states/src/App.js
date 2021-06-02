1f7ee4e0185befd5b6cc92f74e856e369805409e
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-02 21:47:28 +0300

    array states

--- states/src/App.js
+++ states/src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 function App() {
 	const [name, setName] = useState("Mehmet");
 	const [age, setAge] = useState(29);
+	const [friends, setFriends] = useState(["Ahmet", "Murat"]);
 
 	return (
 		<div className="App">
@@ -11,6 +12,20 @@ function App() {
 
 			<button onClick={() => setName("Ahmet")}>Change name</button>
 			<button onClick={() => setAge(25)}>Change age</button>
+
+			<hr />
+			<br></br>
+
+			<h2>Friends</h2>
+			{friends.map((friend, index) => (
+				<div key={index}>{friend}</div>
+			))}
+
+			<br />
+
+			<button onClick={() => setFriends([...friends, "Ayşe"])}>
+				add new friend
+			</button>
 		</div>
 	);
 }
