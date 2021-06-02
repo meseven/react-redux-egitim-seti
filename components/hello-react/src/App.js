79b60b498bcaa508df079e63bb28270c0c124b4e
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-02 04:55:07 +0300

    key prop

--- components/hello-react/src/App.js
+++ components/hello-react/src/App.js
@@ -2,10 +2,31 @@ import "./App.css";
 
 import User from "./components/User";
 
+const friends = [
+	{
+		id: 1,
+		name: "Ahmet",
+	},
+	{
+		id: 2,
+		name: "Tayfun",
+	},
+	{
+		id: 3,
+		name: "Gökhan",
+	},
+];
+
 function App() {
 	return (
 		<>
-			<User name="Mehmet" surname="Seven" isLoggedIn={true} age={29} />
+			<User
+				name="Mehmet"
+				surname="Seven"
+				isLoggedIn={true}
+				age={29}
+				friends={friends}
+			/>
 		</>
 	);
 }
--- components/hello-react/src/components/User.js
+++ components/hello-react/src/components/User.js
@@ -1,6 +1,14 @@
-function User({ name, surname, age, isLoggedIn }) {
+function User({ name, surname, age, isLoggedIn, friends }) {
 	return (
-		<h1>{isLoggedIn ? `${name} ${surname} (${age})` : "Giriş yapmadınız."}</h1>
+		<>
+			<h1>
+				{isLoggedIn ? `${name} ${surname} (${age})` : "Giriş yapmadınız."}
+			</h1>
+
+			{friends.map((friend) => (
+				<div key={friend.id}>{friend.name}</div>
+			))}
+		</>
 	);
 }
 
