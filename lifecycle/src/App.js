db84a5f0e54111e6e59289ccff997300ed509fe0
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-03 00:30:51 +0300

    useEffect

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [number, setNumber] = useState(0);
+	const [name, setName] = useState("Mehmet");
+
+	useEffect(() => {
+		console.log("Component mount edildi!");
+	}, []);
+
+	useEffect(() => {
+		console.log("number state güncellendi!");
+	}, [number]);
+
+	useEffect(() => {
+		console.log("name state güncellendi!");
+	}, [name]);
+
+	return (
+		<div className="App">
+			<h1>{number}</h1>
+			<button onClick={() => setNumber(number + 1)}>Click</button>
+
+			<hr />
+
+			<h1>{name}</h1>
+			<button onClick={() => setName("Mete")}>Click</button>
+		</div>
+	);
 }
 
 export default App;
