c79aeec3fd7aecdeee4c4ce4932e7ce598e74fae
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-03 23:20:21 +0300

    react router installation

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+// pages
+import Home from "./components/Home";
+import About from "./components/About";
+import Users from "./components/Users";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/about">About</Link>
+						</li>
+						<li>
+							<Link to="/users">Users</Link>
+						</li>
+					</ul>
+				</nav>
+
+				<Switch>
+					<Route path="/about" component={About} />
+					<Route path="/users" component={Users} />
+					<Route path="/" component={Home} />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ react-router/src/components/Users.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Users() {
+  return (
+    <div>
+      Users
+    </div>
+  )
+}
+
+export default Users
