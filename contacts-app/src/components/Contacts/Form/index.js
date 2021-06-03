f9a8ce88442ecf044b3defaed696670b5c4145e3
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-03 18:16:16 +0300

    form component

--- contacts-app/src/components/Contacts/Form/index.js
+++ contacts-app/src/components/Contacts/Form/index.js
@@ -1,7 +1,53 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-function Form() {
-	return <div>Form</div>;
+const initialFormValues = { fullname: "", phone_number: "" };
+
+function Form({ addContact, contacts }) {
+	const [form, setForm] = useState(initialFormValues);
+
+	useEffect(() => {
+		setForm(initialFormValues);
+	}, [contacts]);
+
+	const onChangeInput = (e) => {
+		setForm({ ...form, [e.target.name]: e.target.value });
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		if (form.fullname === "" || form.phone_number === "") {
+			return false;
+		}
+
+		addContact([...contacts, form]);
+	};
+
+	return (
+		<form onSubmit={onSubmit}>
+			<div>
+				<input
+					name="fullname"
+					placeholder="Full Name"
+					value={form.fullname}
+					onChange={onChangeInput}
+				/>
+			</div>
+
+			<div>
+				<input
+					name="phone_number"
+					placeholder="Phone Number"
+					value={form.phone_number}
+					onChange={onChangeInput}
+				/>
+			</div>
+
+			<div>
+				<button>Add</button>
+			</div>
+		</form>
+	);
 }
 
 export default Form;
--- contacts-app/src/components/Contacts/index.js
+++ contacts-app/src/components/Contacts/index.js
@@ -1,13 +1,19 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 import List from "./List";
 import Form from "./Form";
 
 function Contacts() {
+	const [contacts, setContacts] = useState([]);
+
+	useEffect(() => {
+		console.log(contacts);
+	}, [contacts]);
+
 	return (
 		<div>
 			<List />
-			<Form />
+			<Form addContact={setContacts} contacts={contacts} />
 		</div>
 	);
 }
