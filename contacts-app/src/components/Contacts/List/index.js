cefa0f8c2cdda822a02d34c75f1e015e1dd5f8bd
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-03 18:37:19 +0300

    list component

--- contacts-app/src/components/Contacts/List/index.js
+++ contacts-app/src/components/Contacts/List/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-function List() {
-	return <div>Contact List</div>;
+function List({ contacts }) {
+	return (
+		<div>
+			<ul>
+				{contacts.map((contact, i) => (
+					<li key={i}>{contact.fullname}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default List;
--- contacts-app/src/components/Contacts/index.js
+++ contacts-app/src/components/Contacts/index.js
@@ -12,7 +12,7 @@ function Contacts() {
 
 	return (
 		<div>
-			<List />
+			<List contacts={contacts} />
 			<Form addContact={setContacts} contacts={contacts} />
 		</div>
 	);
