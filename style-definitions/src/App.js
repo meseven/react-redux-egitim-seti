1d144796f649a45ec52e3816fe408acbaaf2b2b0
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-03 20:03:54 +0300

    inline styles

--- style-definitions/src/App.js
+++ style-definitions/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+
+				<div style={{ color: "red", backgroundColor: "white", paddingTop: 50 }}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel eos sunt
+					omnis quaerat ipsam maxime hic dolor, similique error molestias nam
+					facere explicabo cum unde quod officia praesentium? Numquam,
+					doloribus?
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
