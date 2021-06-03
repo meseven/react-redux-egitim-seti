fcabc8af587f6df36c89541aafa801530e3f9f58
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-03 20:36:13 +0300

    module.css

--- style-definitions/src/App.js
+++ style-definitions/src/App.js
@@ -1,19 +1,14 @@
 import "./App.css";
 
+import A from "./components/A";
+import B from "./components/B";
+
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-
-				<div style={{ color: "red", backgroundColor: "white", paddingTop: 50 }}>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel eos sunt
-					omnis quaerat ipsam maxime hic dolor, similique error molestias nam
-					facere explicabo cum unde quod officia praesentium? Numquam,
-					doloribus?
-				</div>
+				<A />
+				<B />
 			</header>
 		</div>
 	);
