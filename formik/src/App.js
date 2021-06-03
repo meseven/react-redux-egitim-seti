822153c700732e8e5f9201c83e1030a248bd6637
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-04 00:57:32 +0300

    formik installation

--- formik/src/App.js
+++ formik/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { Formik, Field, Form } from "formik";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Sign Up</h1>
+			<Formik
+				initialValues={{
+					firstName: "",
+					lastName: "",
+					email: "",
+				}}
+				onSubmit={(values) => {
+					console.log(values);
+				}}
+			>
+				<Form>
+					<label htmlFor="firstName">First Name</label>
+					<Field name="firstName" placeholder="Jane" />
+
+					<br />
+					<br />
+
+					<label htmlFor="lastName">Last Name</label>
+					<Field name="lastName" placeholder="Doe" />
+
+					<br />
+					<br />
+
+					<label htmlFor="email">Email</label>
+					<Field name="email" placeholder="jane@acme.com" type="email" />
+
+					<br />
+					<br />
+
+					<button type="submit">Submit</button>
+				</Form>
+			</Formik>
+		</div>
+	);
 }
 
 export default App;
