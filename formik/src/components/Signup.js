c9bcfb1001bcfb2f32731a2837abb6cad3d19e3d
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-04 17:02:54 +0300

    error messages

--- formik/src/components/Signup.js
+++ formik/src/components/Signup.js
@@ -3,24 +3,34 @@ import { useFormik } from "formik";
 import validationSchema from "./validations";
 
 function Signup() {
-	const { handleSubmit, handleChange, values } = useFormik({
-		initialValues: {
-			email: "",
-			password: "",
-			passwordConfirm: "",
-		},
-		onSubmit: (values) => {
-			console.log(values);
-		},
-		validationSchema,
-	});
+	const { handleSubmit, handleChange, handleBlur, values, errors, touched } =
+		useFormik({
+			initialValues: {
+				email: "",
+				password: "",
+				passwordConfirm: "",
+			},
+			onSubmit: (values) => {
+				console.log(values);
+			},
+			validationSchema,
+		});
 
 	return (
 		<div>
 			<h1>Sign Up</h1>
 			<form onSubmit={handleSubmit}>
 				<label>Email</label>
-				<input name="email" value={values.email} onChange={handleChange} />
+				<input
+					name="email"
+					value={values.email}
+					onChange={handleChange}
+					onBlur={handleBlur}
+				/>
+
+				{errors.email && touched.email && (
+					<div className="error">{errors.email}</div>
+				)}
 
 				<br />
 				<br />
@@ -30,7 +40,12 @@ function Signup() {
 					name="password"
 					value={values.password}
 					onChange={handleChange}
+					onBlur={handleBlur}
 				/>
+
+				{errors.password && touched.password && (
+					<div className="error">{errors.password}</div>
+				)}
 
 				<br />
 				<br />
@@ -40,7 +55,12 @@ function Signup() {
 					name="passwordConfirm"
 					value={values.passwordConfirm}
 					onChange={handleChange}
+					onBlur={handleBlur}
 				/>
+
+				{errors.passwordConfirm && touched.passwordConfirm && (
+					<div className="error">{errors.passwordConfirm}</div>
+				)}
 
 				<br />
 				<br />
--- formik/src/components/validations.js
+++ formik/src/components/validations.js
@@ -1,12 +1,18 @@
 import * as yup from "yup";
 
 const validations = yup.object().shape({
-	email: yup.string().email().required(),
-	password: yup.string().min(5).required(),
+	email: yup
+		.string()
+		.email("Geçerli bir email girin.")
+		.required("Zorunlu alan."),
+	password: yup
+		.string()
+		.min(5, "Parolanız en az 5 karakter olmalıdır.")
+		.required("Zorunlu alan."),
 	passwordConfirm: yup
 		.string()
-		.oneOf([yup.ref("password")])
-		.required(),
+		.oneOf([yup.ref("password")], "Parolalar uyuşmuyor")
+		.required("Zorunlu alan."),
 });
 
 export default validations;
