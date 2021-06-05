ad998ae788ed0daf4fb510b843e600f5c2867db8
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-05 19:47:24 +0300

    useCallback

--- memoization/src/components/Header.js
+++ memoization/src/components/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-function Header({ number, data }) {
+function Header({ number, increment }) {
 	console.log("Header Component Re-rendered!");
+
 	return (
 		<div>
 			Header - {number}
 			<br />
 			<br />
-			<code>{JSON.stringify(data)}</code>
+			<button onClick={increment}>Click</button>
 		</div>
 	);
 }
