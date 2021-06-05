dd0c6aa2cb86604732bd2f68d99a3e859c9d90f0
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-05 18:59:55 +0300

    useMemo hook

--- memoization/src/App.js
+++ memoization/src/App.js
@@ -1,21 +1,40 @@
 import "./App.css";
 
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import Header from "./components/Header";
 
 function App() {
 	const [number, setNumber] = useState(0);
+	const [text, setText] = useState("");
+
+	const data = useMemo(() => {
+		return calculateObject(number);
+	}, [number]);
+
+	// const data = calculateObject();
 
 	return (
 		<div className="App">
-			<Header number={number < 5 ? 0 : number} />
+			<Header data={data} />
 
 			<hr />
 
 			<h1>{number}</h1>
 			<button onClick={() => setNumber(number + 1)}>Click</button>
+
+			<br />
+			<br />
+			<input value={text} onChange={({ target }) => setText(target.value)} />
 		</div>
 	);
+}
+
+function calculateObject(number) {
+	console.log("Calculating...");
+	for (let i = 0; i < 1000000000; i++) {}
+	console.log("Calculating completed!");
+
+	return { name: "Mehmet", number };
 }
 
 export default App;
--- memoization/src/components/Header.js
+++ memoization/src/components/Header.js
@@ -1,8 +1,15 @@
 import React from "react";
 
-function Header({ number }) {
+function Header({ number, data }) {
 	console.log("Header Component Re-rendered!");
-	return <div>Header - {number}</div>;
+	return (
+		<div>
+			Header - {number}
+			<br />
+			<br />
+			<code>{JSON.stringify(data)}</code>
+		</div>
+	);
 }
 
 export default React.memo(Header);
