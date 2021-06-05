3eecf2fc2da90d77faee5f0ad14fc9e7440603a9
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-05 22:43:06 +0300

    side effects

--- context/src/context/ThemeContext.js
+++ context/src/context/ThemeContext.js
@@ -1,9 +1,13 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-	const [theme, setTheme] = useState("dark");
+	const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+	useEffect(() => {
+		localStorage.setItem("theme", theme);
+	}, [theme]);
 
 	const values = {
 		theme,
