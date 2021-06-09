30b28a0288cca58e60d51548f98d7945a5c8a0ec
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-10 01:51:45 +0300

    todo component unit testing

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 
-import Counter from "./components/Counter";
+// import Counter from "./components/Counter";
+import Todo from "./components/Todo";
 
 function App() {
 	return (
 		<div className="App">
-			<Counter />
+			{/* <Counter /> */}
+			<Todo />
 		</div>
 	);
 }
