967f188f92c05c6c0dedefeabc08c1ccbfaac94d
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-09 16:41:15 +0300

    localization

--- localization/src/App.js
+++ localization/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState } from "react";
+import { IntlProvider, FormattedMessage, FormattedNumber } from "react-intl";
+
+const messages = {
+	"tr-TR": {
+		title: "Merhaba Dünya",
+		description: "3 yeni mesajınız var",
+	},
+	"en-US": {
+		title: "Hello World",
+		description: "You have 3 new messages",
+	},
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [lang, setLang] = useState("tr-TR");
+
+	return (
+		<div className="App">
+			<IntlProvider messages={messages[lang]}>
+				<FormattedMessage id="title" />
+
+				<p>
+					<FormattedMessage id="description" />
+				</p>
+
+				<br />
+				<br />
+				<button onClick={() => setLang("tr-TR")}>TR</button>
+				<button onClick={() => setLang("en-US")}>EN</button>
+			</IntlProvider>
+		</div>
+	);
 }
 
 export default App;
