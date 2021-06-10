1c0ef2f553fb0838f614e664835d34e4a3010aea
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-11 00:27:44 +0300

    feed scroll

--- realtime/chat-app/client/src/components/ChatList.js
+++ realtime/chat-app/client/src/components/ChatList.js
@@ -3,16 +3,18 @@ import styles from "./styles.module.css";
 import { useChat } from "../context/ChatContext";
 import ChatItem from "./ChatItem";
 
+import ScrollableFeed from "react-scrollable-feed";
+
 function ChatList() {
 	const { messages } = useChat();
 
 	return (
 		<div className={styles.chatlist}>
-			<div>
+			<ScrollableFeed forceScroll={true}>
 				{messages.map((item, key) => (
 					<ChatItem key={key} item={item} />
 				))}
-			</div>
+			</ScrollableFeed>
 		</div>
 	);
 }
