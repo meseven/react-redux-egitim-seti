3fbe2ba90d631dadf1e9b00623c65d6a7caa80ec
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-10 20:21:07 +0300

    realtime colors app ui

---
+++ realtime/realtime-colors/client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Palatte from "./components/Palatte";
+
+function App() {
+	return (
+		<div className="App">
+			<Palatte />
+		</div>
+	);
+}
+
+export default App;
---
+++ realtime/realtime-colors/client/src/components/Palatte.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Palatte() {
+	return (
+		<div className="palette">
+			<input type="color" />
+			<button>Click</button>
+		</div>
+	);
+}
+
+export default Palatte;
