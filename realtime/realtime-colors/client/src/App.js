fd170676f7c3b05f7469b7d22aa8bef4b6aced2f
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-10 20:30:58 +0300

    backend connection

--- realtime/realtime-colors/client/src/App.js
+++ realtime/realtime-colors/client/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+
+import { useEffect } from "react";
+import { init } from "./socketApi";
 import Palatte from "./components/Palatte";
 
 function App() {
+	useEffect(() => {
+		init();
+	}, []);
+
 	return (
 		<div className="App">
 			<Palatte />
---
+++ realtime/realtime-colors/client/src/socketApi.js
@@ -0,0 +1,15 @@
+import io from "socket.io-client";
+
+let socket;
+
+export const init = () => {
+	console.log("Sunucuya bağlanılıyor...");
+
+	socket = io("http://localhost:3001", {
+		transports: ["websocket"],
+	});
+
+	socket.on("connect", () =>
+		console.log("Sunucuya bağlantı başarıyla gerçekleşti.")
+	);
+};
