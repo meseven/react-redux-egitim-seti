5c9f67d7cb2558f1bdef07830e7131d64f902ff1
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-10 23:46:17 +0300

    backend connection

--- realtime/chat-app/client/src/components/Container.js
+++ realtime/chat-app/client/src/components/Container.js
@@ -1,9 +1,15 @@
-import React from "react";
+import { useEffect } from "react";
 
 import ChatList from "./ChatList";
 import ChatForm from "./ChatForm";
 
+import { init } from "../socketApi";
+
 function Container() {
+	useEffect(() => {
+		init();
+	}, []);
+
 	return (
 		<div className="App">
 			<ChatList />
---
+++ realtime/chat-app/client/src/socketApi.js
@@ -0,0 +1,13 @@
+import io from "socket.io-client";
+
+let socket;
+
+export const init = () => {
+	console.log("Connecting...");
+
+	socket = io("http://localhost:3000", {
+		transports: ["websocket"],
+	});
+
+	socket.on("connect", () => console.log("Connected!"));
+};
