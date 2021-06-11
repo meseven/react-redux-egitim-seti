55633b721c12f1b44bc1c83d22fc7aa96ad57198
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-11 14:43:28 +0300

    intro

--- intro/index.js
+++ intro/index.js
@@ -1,5 +1,8 @@
-var slugify = require("slugify");
+import merhaba, { topla, cikar, text, user, users } from "./my-module.js";
 
-const title = slugify("some string lorem ipsum", "*");
-
-console.log(title);
+console.log(topla(2, 4));
+console.log(cikar(4, 2));
+console.log(text);
+console.log(user);
+console.log(users);
+merhaba("Mehmet");
