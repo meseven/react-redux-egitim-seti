5552a026eaa25064476beda4fed622cf14a9852c
Mehmet Seven <mehmetseven0@gmail.com>
2021-06-17 17:37:53 +0300

    react query refetchOnMount

--- ecommerce-app/client/src/index.js
+++ ecommerce-app/client/src/index.js
@@ -4,21 +4,29 @@ import "./index.css";
 import "./reset.css";
 
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnMount: false,
+			refetchOnWindowFocus: false,
+		},
+	},
+});
 
 ReactDOM.render(
-	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<ChakraProvider>
-				<App />
-			</ChakraProvider>
-		</QueryClientProvider>
-	</React.StrictMode>,
+	<QueryClientProvider client={queryClient}>
+		<ChakraProvider>
+			<App />
+		</ChakraProvider>
+
+		<ReactQueryDevtools initialIsOpen={false} />
+	</QueryClientProvider>,
 	document.getElementById("root")
 );
 
